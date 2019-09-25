2f1af30569005df94903d8b8219d1f453725300a
Nana Adjei Manu <claeusdev@gmail.com>
2019-09-25 08:20:42 -0700

    refactor models

--- app/models/band.js
+++ app/models/band.js
@@ -1,15 +1,12 @@
-import EmberObject, { computed } from "@ember/object";
+import DS from "ember-data";
+import { computed } from "@ember/object";
 import { dasherize } from "@ember/string";
 
-export default EmberObject.extend({
-  name: "",
-  description: "",
-  init() {
-    this._super(...arguments);
-    if (!this.songs) {
-      this.set("songs", []);
-    }
-  },
+const { Model, attr, hasMany } = DS;
+export default Model.extend({
+  name: attr("string"),
+  description: attr("string"),
+  songs: hasMany(),
   slug: computed("name", function() {
     return dasherize(this.name);
   })
